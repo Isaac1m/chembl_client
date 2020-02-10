c307815f2bdf7f31d55459ad2f9e5d72bc83754f
Isaac Jandalala <ijandalala@rbm.malawi.net>
2020-02-10 09:54:53 +0200

    Filter activities keeping those for specified molecules and the pchembl value is not null.

--- src/scripts/getData.js
+++ src/scripts/getData.js
@@ -1,32 +1,69 @@
 const axios = require('axios');
 const fs = require('fs');
 
-function writeToFile(activities) {
 
-    let data = JSON.stringify(activities, null, 2);
+let targets = [
+    "CHEMBL325",
+    "CHEMBL1937",
+    "CHEMBL1829",
+    "CHEMBL3524",
+    "CHEMBL2563",
+    "CHEMBL1865",
+    "CHEMBL2716",
+    "CHEMBL3192",
+    "CHEMBL4145",
+    "CHEMBL5103",
+    "CHEMBL3310"
+];
 
-    fs.writeFile('../../data/db.json', data, (err) => {
-        if (err) throw err;
-        console.log('Data written to file');
-    });
+let molecules = [
+    "CHEMBL98",
+    "CHEMBL99",
+    "CHEMBL27759",
+    "CHEMBL2018302",
+    "CHEMBL483254",
+    "CHEMBL1213490",
+    "CHEMBL356769",
+    "CHEMBL272980",
+    "CHEMBL430060",
+    "CHEMBL1173445",
+    "CHEMBL356066",
+    "CHEMBL1914702"
+];
 
-    console.log('This is after the write call');
+let activities = [];
+
+
+
+let writeToFile = (activities) => {
+    activities.map(activity => {
+        let data = JSON.stringify(activity, null, 2);
+        fs.appendFile('../../data/db.json', data, (err) => {
+            if (err) throw err;
+            console.log(`Activity written to file`);
+        });
+    })
+
 }
 
-function getData() {
-    //test endpoint
-    axios
-        .get(
-            `https://www.ebi.ac.uk/chembl/api/data/activity.json?target_chembl_id=CHEMBL325`
+let getData = () => {
+    targets.map(target => {
+        axios.get(
+            `https://www.ebi.ac.uk/chembl/api/data/activity.json?target_chembl_id=${target}`
         )
-        .then(response => {
-            const { activities } = response.data;
-            writeToFile(activities);
-            // console.log(activities);
-        })
-        .catch(error => {
-            console.error(`Error fetching activities: ${error}`);
-        });
+            .then(response => {
+                response.data.activities.map(activity => {
+                    if (molecules.includes(activity.molecule_chembl_id) && activity.pchembl_value != null) {
+                        activities.push(activity)
+                    }
+                })
+
+                writeToFile(activities);
+            })
+            .catch(error => {
+                console.error(`Error fetching activities: ${error}`);
+            });
+    })
 }
 
 
